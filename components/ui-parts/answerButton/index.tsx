7c0cfe1be405978ac79bdc7b1b718021b2f7be2c
tak_kojima <takuto9119@gmail.com>
2023-05-28 17:22:40 +0900

    回答ボタンコンポーネントの追加

---
+++ components/ui-parts/answerButton/index.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+import Button from '../button'
+
+const AnswerButton = () => {
+    const onClick = () => {}
+    return (
+        <Button text="回答" onClick={onClick} />
+    )
+}
+
+export default AnswerButton;
--- pages/sample.tsx
+++ pages/sample.tsx
@@ -1,11 +1,14 @@
 import FletBorad from "../components/ui-parts/fletboard";
+import AnswerButton from "../components/ui-parts/answerButton";
 import {PressFletMarksProvider} from "../components/context/PressFletMarksContext";
+import "../styles/font.css";
 
 const Sample = () => {
     return(
        <>
        <PressFletMarksProvider>
        <FletBorad />
+       <AnswerButton />
        </PressFletMarksProvider>
        </>
     )
