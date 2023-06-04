2a4aed67bd76dc3b61293e4ffc6c4087b15a81d4
tak_kojima <takuto9119@gmail.com>
2023-06-05 01:43:56 +0900

    環境変数の追加、各種設定の修正、定数ファイル追加

---
+++ components/const/const.ts
@@ -0,0 +1,6 @@
+export const STRING_COUNT = 6 as const // 弦の本数
+export const FLET_COUNT = 24 as const // 一つの弦あたりのフレットの数
+export const OPEN_FLET_NUM = 0 as const // 開放弦のフレット
+export const FIRST_FLET = 1 as const // 最初のフレットの位置
+export const FOURTH_STRING = 4 as const // 4フレット目の位置
+export const MARKING_FLET_NUM = [3, 5, 7, 9, 12, 15, 18, 21, 24] as const // フレットボード上の特定の弦のマークの位置
